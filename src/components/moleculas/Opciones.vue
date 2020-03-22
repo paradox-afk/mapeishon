051321256e68ee4d2fceaa3f9b3726bd82443426
memolokokj <62449690+memolokokj@users.noreply.github.com>
2020-03-21 21:10:14 -0700

    Add files via upload
    asd2

---
+++ src/components/moleculas/Opciones.vue
@@ -0,0 +1,63 @@
+<template>
+    <div id="cosa" :name="name" :index="index">
+      <button @click="emitir">
+        <div id="contenedor">
+            <div id="imag"><img :src="imagen"></div>
+            <div id="text"><h2>{{name}}</h2></div>
+        </div>
+    </button>
+    </div>
+</template>
+
+<script>
+export default {
+    props:["name","accion","index","imagen"],
+    methods:
+    { 
+        emitir()
+        {
+            this.$emit("emitiro",this.index);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #cosa
+    {
+        width: 300px;
+        height: 90px;
+    }
+    #contenedor
+    {
+        float: left;
+        width: 100%;
+        height: 100%;
+    }
+    #imag
+    {
+        float: left;
+        width: 30%;
+        height: 100%;
+    }
+    #text
+    {
+        float: left;
+        width: 40%;
+        height: 100%;
+        padding-top: 5%;
+        padding-left: 10%;
+    }
+    img
+    {
+        padding-top: 5%;
+        width: 80px;
+        height: 80px;   
+    }
+    button
+    {
+        width: 100%;
+        height: 100px;
+    }
+
+</style>
