8c324b05df15f5973616644a4201fe218b82e138
memolokokj <62449690+memolokokj@users.noreply.github.com>
2020-03-21 20:55:12 -0700

    hola

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="map-view">
-     <Mapa id="mapita" :longitud="longitud " :latitud="latitud" :descrip="descrip" :name="name" :imagen="imagen"/>
-     <Card styles="top: 10px; left: 10px;">
-    <BarraBusqueda @emitir="recibir"/>
+     <Mapa id="mapita" :longitud="longitud2 " :latitud="latitud2" :descrip="descrip2" :name="name2" :imagen="imagen2"/>
+     <Card styles="top: 10px; left: 5px;">
+        <BarraBusqueda @emitir="recibir"/>
+     </Card>
+     <Card v-if="hack.length!=0" :styles="cuadro">
+        <Opciones v-for="(nem,index) in name" :key="nem+index" :index="index" :name="nem" @emitir="seleccion" :imagen="imagen[index]"/>
      </Card>
   </div>
 </template>
 
 <script>
 import Mapa from "../components/moleculas/Mapa";
+import Opciones from "../components/moleculas/Opciones";
 import Card from "../components/atomos/Card";
 import BarraBusqueda from "../components/moleculas/BarraBusqueda";
 export default {
@@ -16,27 +20,75 @@ export default {
     components: {
         Mapa,
         Card,
-        BarraBusqueda
+        BarraBusqueda,
+        Opciones
     },
     data(){
         return{
-            longitud:"",
-            latitud:"",
-            descrip:"",
-            name:"",
-            imagen:""
+            longitud:[],
+            latitud:[],
+            descrip:[],
+            name:[],
+            imagen:[],
+            bandera:false,
+            longitud2:"",
+            latitud2:"",
+            descrip2:"",
+            name2:"",
+            imagen2:"",
+            hack:[],
+            cuadro:"top: 50px; left: 5px; height: 365px;",
         };
     },
-methods:{
-    recibir(response)
+    methods:
     {
-        this.longitud = response.data[0].alt;
-        this.latitud = response.data[0].lat;
-        this.descrip = response.data[0].descripcion;
-        this.name = response.data[0].name;
-        this.imagen = response.data[0].imagen;
+        recibir(response)
+        {
+            this.hack = response;
+            this.longitud = [];
+            this.latitud = [];
+            this.descrip = [];
+            this.name = [];
+            this.imagen = [];
+
+            for(var i=0;i<response.data.length;i++)
+            {
+                this.longitud.push(response.data[i].alt);
+                this.latitud.push(response.data[i].lat);
+                this.descrip.push(response.data[i].descripcion);
+                this.name.push(response.data[i].name);
+                this.imagen.push(response.data[i].imagen);
+            }
+
+            if(response.data.length >= 4)
+            {
+                this.cuadro = "top: 50px; left: 5px; height: 365px;";
+            }
+            else
+            {
+                switch(response.data.length)
+                {
+                    case 1:
+                        this.cuadro = "top: 50px; left: 5px; height: 110px;";
+                        break;
+                    case 2:
+                        this.cuadro = "top: 50px; left: 5px; height: 200px;";
+                        break;
+                    case 3:
+                        this.cuadro = "top: 50px; left: 5px; height: 290px;";
+                        break;
+                }
+            }
+        },
+        seleccion(index)
+        {
+            this.longitud2 = this.hack.data[index].alt;
+            this.latitud2 = this.hack.data[index].lat;
+            this.descrip2 = this.hack.data[index].descripcion;
+            this.name2 = this.hack.data[index].name;
+            this.imagen2 = this.hack.data[index].imagen;
+        }
     }
-}
 }
 </script>
 
